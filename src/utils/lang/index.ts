e6fefb1f81c096193bcc0445cd7cb89fff2470fc
carefree0910 <syameimaru.saki@gmail.com>
2023-04-06 16:29:44 +0800

    ✨Use `en` as default `lang`

--- src/utils/lang/index.ts
+++ src/utils/lang/index.ts
@@ -1,11 +1,12 @@
 import { Lang } from "@noli/core";
-import { langDescriptions, updateDictionary } from "@noli/business";
+import { langDescriptions, langStore, updateDictionary } from "@noli/business";
 
 import { toastLangRecords } from "./toast";
 
 const initLangDirs = [toastLangRecords];
 
 export function initializeLang(): void {
+  langStore.updateProperty("tgt", "en");
   for (const lang of Object.keys(langDescriptions) as Lang[]) {
     initLangDirs.forEach((d) => updateDictionary(lang, d[lang]));
   }
