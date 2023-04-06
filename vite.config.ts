b343ec3e3ddfbcd42affae7b8af406718f7d4408
carefree0910 <syameimaru.saki@gmail.com>
2023-04-06 16:29:29 +0800

    🔨Updated `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    include: ["@noli/core", "@noli/business"],
+  },
 });
