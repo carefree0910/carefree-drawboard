f7496a7b88b5bb188a8b09db40c9ac9eea4c62a1
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2023-04-06 08:33:55 +0800

    ✏️Changed some lang

--- src/utils/lang/toast.ts
+++ src/utils/lang/toast.ts
@@ -13,7 +13,7 @@ export const toastLangRecords: Record<Lang, Record<Toast_Words, string>> = {
   zh: {
     [Toast_Words["dropping-message"]]: "识别中 🤔",
     [Toast_Words["uploading-message"]]: "上传中，请稍候 😄",
-    [Toast_Words["upload-image-success-message"]]: "上传成功！🥳",
+    [Toast_Words["upload-image-success-message"]]: "上传图片成功！🥳",
     [Toast_Words["upload-image-error-message"]]: "上传图片失败 😫",
     [Toast_Words["upload-image-not-safe-warning-message"]]: "您上传的图片未通过审核 😫",
     [Toast_Words["strange-image-error-message"]]: "图片类型错误，当前仅支持上传 jpg/png 图片 😫",
@@ -21,7 +21,7 @@ export const toastLangRecords: Record<Lang, Record<Toast_Words, string>> = {
   en: {
     [Toast_Words["dropping-message"]]: "Detecting 🤔",
     [Toast_Words["uploading-message"]]: "Uploading, please wait for a while 😄",
-    [Toast_Words["upload-image-success-message"]]: "Uploaded Successfully! 🥳",
+    [Toast_Words["upload-image-success-message"]]: "Upload image successfully! 🥳",
     [Toast_Words["strange-image-error-message"]]: "Only jpg/png images are supported 😫",
     [Toast_Words["upload-image-error-message"]]: "Upload image failed 😫",
     [Toast_Words["upload-image-not-safe-warning-message"]]:
