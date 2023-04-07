221d23174bf05624c5d8dfdabf4f6646f40dbaff
carefree0910 <syameimaru.saki@gmail.com>
2023-04-07 14:17:33 +0800

    ✨Implemented `CFIcon`

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,49 @@
+/// <reference types="react-scripts" />
+
+declare module "*.avif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.bmp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
+  const src: string;
+  export default src;
+}
+
+declare module "qrcode.react";
+
+declare module "fabric";
