8a1855dc3571c8ecbbd243854cc202c58dff9f30
carefree0910 <syameimaru.saki@gmail.com>
2023-04-07 14:16:20 +0800

    🔨Updated `package.json` and some settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
   optimizeDeps: {
     include: ["@noli/core", "@noli/business"],
   },
