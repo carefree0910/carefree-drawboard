f56b35302a715acc46383592e5e2d66bb6c39eb4
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2023-04-09 09:42:26 +0800

    ✨Introduced `PYTHON_RELATED_SETTINGS`

--- src/requests/apis.ts
+++ src/requests/apis.ts
@@ -1,8 +1,12 @@
 import axios, { AxiosInstance } from "axios";
 
 import type { APISources } from "@/types/requests";
+import { PYTHON_RELATED_SETTINGS } from "@/utils/constants";
 
 export const apis: Record<APISources, AxiosInstance> = {
   nolibox: axios.create({ baseURL: "https://creator-huawei-test.nolibox.com", timeout: 3000 }),
-  _python: axios.create({ baseURL: "http://localhost:8123", timeout: 300000 }),
+  _python: axios.create({
+    baseURL: `http://localhost:${PYTHON_RELATED_SETTINGS.backendPort}`,
+    timeout: 300000,
+  }),
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -17,3 +17,7 @@ export const DEFAULT_PLUGIN_SETTINGS = {
   expandOffsetX: 8,
   expandOffsetY: 8,
 };
+
+export const PYTHON_RELATED_SETTINGS = {
+  backendPort: 8123,
+};
