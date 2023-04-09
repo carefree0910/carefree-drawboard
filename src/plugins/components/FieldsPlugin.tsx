ff3a80c9c4096af1f06931dc2c35270ec935714a
carefree0910 <syameimaru.saki@gmail.com>
2023-04-09 11:08:31 +0800

    ✨Extracted `FieldsPlugin`

---
+++ src/plugins/components/FieldsPlugin.tsx
@@ -0,0 +1,43 @@
+import { useMemo } from "react";
+import { observer } from "mobx-react-lite";
+
+import type { IInternalTaskPlugin } from "@/types/plugins";
+import type { ISubscribableFields } from "@/types/metaFields";
+import { subscribe } from "../utils/subscribe";
+import Render from "./Render";
+import TextField from "./TextField";
+
+const FieldsPlugin = ({
+  pluginInfo: { fields, customDefinitions },
+  children,
+  ...props
+}: Omit<IInternalTaskPlugin, "pluginInfo"> & {
+  pluginInfo: Omit<IInternalTaskPlugin["pluginInfo"], "task">;
+}) => {
+  const definitions = useMemo(
+    () => subscribe(fields, customDefinitions),
+    [fields, customDefinitions],
+  );
+
+  return (
+    <Render {...props}>
+      {Object.entries(definitions).map(([field, definition]) => {
+        if (definition.type === "text") {
+          return (
+            <TextField
+              key={field}
+              field={field as ISubscribableFields}
+              definition={definition}
+              flexShrink={0}
+              {...definition.props}
+            />
+          );
+        }
+        return null;
+      })}
+      {children}
+    </Render>
+  );
+};
+
+export default observer(FieldsPlugin);
--- src/plugins/components/TaskPlugin.tsx
+++ src/plugins/components/TaskPlugin.tsx
@@ -5,22 +5,13 @@ import { Button, useToast } from "@chakra-ui/react";
 import { langStore } from "@noli/business";
 
 import type { IInternalTaskPlugin } from "@/types/plugins";
-import type { ISubscribableFields } from "@/types/metaFields";
 import { themeStore } from "@/stores/theme";
 import { importMeta } from "@/actions/importMeta";
 import { CFDivider } from "@/components/CFDivider";
-import { subscribe } from "../utils/subscribe";
-import Render from "./Render";
-import TextField from "./TextField";
+import FieldsPlugin from "./FieldsPlugin";
 
-const TaskPlugin = ({
-  pluginInfo: { task, fields, customDefinitions },
-  ...props
-}: IInternalTaskPlugin) => {
-  const definitions = useMemo(
-    () => subscribe(fields, customDefinitions),
-    [fields, customDefinitions],
-  );
+const TaskPlugin = (props: IInternalTaskPlugin) => {
+  const { task } = props.pluginInfo;
   const onSubmit = useMemo(() => () => importMeta({ t, lang, type: task }), [task]);
 
   const t = useToast();
@@ -28,26 +19,12 @@ const TaskPlugin = ({
   const { textColor } = themeStore.styles;
 
   return (
-    <Render {...props}>
-      {Object.entries(definitions).map(([field, definition]) => {
-        if (definition.type === "text") {
-          return (
-            <TextField
-              key={field}
-              field={field as ISubscribableFields}
-              definition={definition}
-              flexShrink={0}
-              {...definition.props}
-            />
-          );
-        }
-        return null;
-      })}
+    <FieldsPlugin {...props}>
       <CFDivider />
       <Button color={textColor} flexShrink={0} onClick={onSubmit}>
         Submit
       </Button>
-    </Render>
+    </FieldsPlugin>
   );
 };
 
