e8f3a5b595a8efae8dba336405c1eb6949a7e95c
carefree0910 <syameimaru.saki@gmail.com>
2023-04-09 10:08:24 +0800

    📝Added comment to `_python`

--- src/panels/_python.ts
+++ src/panels/_python.ts
@@ -2,6 +2,15 @@ import type { IMakePythonPlugin } from "@/types/_python";
 import type { AvailablePythonPlugins } from "@/types/plugins";
 
 export const pythonPluginSettings: IMakePythonPlugin<AvailablePythonPlugins>[] = [
+  /**
+   * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   * !!!!!                                 Please use                                   !!!!!
+   * !!!!!                                                                              !!!!!
+   * !!!!!          `git update-index --skip-worktree ./src/panels/_python.ts`          !!!!!
+   * !!!!!                                                                              !!!!!
+   * !!!!!                       to ignore changes of this file                         !!!!!
+   * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   */
   // START PYTHON PLUGIN SETTINGS
   // END PYTHON PLUGIN SETTINGS
 ];
