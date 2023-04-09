05156284eea1aacf676e8a53c13b3dcf26fd91f5
carefree0910 <syameimaru.saki@gmail.com>
2023-04-09 10:29:56 +0800

    ✨Extracted `_parse_dict`

--- cfdraw/parsers/utils.py
+++ cfdraw/parsers/utils.py
@@ -2,29 +2,53 @@ import json
 
 from typing import Any
 from typing import Dict
+from typing import List
+from typing import Optional
+from typing import NamedTuple
 
 from cfdraw import constants
 
 
-def parse_dict_from(pivot: str, path: str) -> Dict[str, Any]:
+class ParseResponse(NamedTuple):
+    d: Dict[str, Any]
+    start_idx: int
+    end_idx: int
+    all_lines: Optional[List[str]]
+
+
+def _parse_dict(pivot: str, path: str, require_all_lines: bool) -> ParseResponse:
     with open(path, "r") as f:
         start = False
+        all_lines = [] if require_all_lines else None
         target_lines = []
-        for line in f:
+        start_idx = end_idx = 0
+        for i, line in enumerate(f):
+            if all_lines is not None:
+                all_lines.append(line)
             line = line.strip()
             if line.startswith(pivot):
                 start = True
+                start_idx = i
                 target_lines.append("{")
             elif start:
                 if line.endswith("};"):
                     target_lines[-1] = target_lines[-1][:-1]  # strip the trailing comma
                     target_lines.append("}")
-                    break
-                left, right = line.split(": ")
-                line = f'"{left}": {right}'
-                target_lines.append(line)
+                    end_idx = i
+                    start = False
+                    if not require_all_lines:
+                        break
+                else:
+                    left, right = line.split(": ")
+                    line = f'"{left}": {right}'
+                    target_lines.append(line)
     json_str = "".join(target_lines)
-    return json.loads(json_str)
+    d = json.loads(json_str)
+    return ParseResponse(d, start_idx, end_idx, all_lines)
+
+
+def parse_dict_from(pivot: str, path: str) -> Dict[str, Any]:
+    return _parse_dict(pivot, path, False).d
 
 
 def parse_dict_from_ts_constants(pivot: str) -> Dict[str, Any]:
