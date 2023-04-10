cd7440dd0b4a1cf19852673792ef7052a44dced8
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2023-04-10 09:23:49 +0800

    ✨Introduced `register_all_available_plugins`

--- cfdraw/__init__.py
+++ cfdraw/__init__.py
@@ -5,3 +5,8 @@ from cfdraw.plugins import *
 
 register_plugin = PluginFactory.register
 available_plugins = PluginFactory.available
+
+
+def register_all_available_plugins() -> None:
+    for name, info in available_plugins().items():
+        register_plugin(name)(info.plugin_type)
