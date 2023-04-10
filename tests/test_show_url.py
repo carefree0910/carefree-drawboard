0f32083d138b1bf9022a7ae3cb05ec89d22c12e1
carefree0910 <syameimaru.saki@gmail.com>
2023-04-10 11:28:29 +0800

    ✅Introduced `test_show_url`

---
+++ tests/test_show_url.py
@@ -0,0 +1,27 @@
+from cfdraw import *
+
+
+class HttpShowUrlPlugin(IHttpPlugin):
+    @property
+    def type(self) -> PluginType:
+        return PluginType.HTTP_TEXT_AREA
+
+    @property
+    def settings(self) -> IPluginSettings:
+        return IPluginSettings(
+            w=300,
+            h=100,
+            nodeConstraint=NodeConstraints.IMAGE,
+            src="https://ailab-huawei-cdn.nolibox.com/upload/images/effd9650ce934b239242c6122b701514.png",
+            follow=True,
+            pivot=PivotType.RT,
+            offsetY=-48,
+            expandOffsetY=-100,
+        )
+
+    def process(self, data: IHttpPluginRequest) -> str:
+        return data.nodeData.src or "Not Found"
+
+
+register_plugin("show_url")(HttpShowUrlPlugin)
+app = App()
