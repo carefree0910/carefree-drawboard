7e5d64596b6e1c48b5ec07625bafe71614ba035f
carefree0910 <syameimaru.saki@gmail.com>
2023-04-10 14:27:14 +0800

    ⚡️🐛✨Enhanced `set_constants`
    To enable hot reload & prevent page reload

--- cfdraw/compilers/settings.py
+++ cfdraw/compilers/settings.py
@@ -4,11 +4,20 @@ from typing import Any
 from typing import Dict
 
 from cfdraw import constants
+from cfdraw.parsers.utils import parse_dict_from
 from cfdraw.compilers.utils import get_file_info
 from cfdraw.compilers.utils import set_file_info
 
 
 def set_constants(updates: Dict[str, Any]) -> None:
+    existing_d = parse_dict_from("return {", constants.TS_PYTHON_CONSTANTS_PATH)
+    need_update = False
+    for k, v in updates.items():
+        if existing_d.get(k) != v:
+            need_update = True
+            break
+    if not need_update:
+        return
     info = get_file_info(
         constants.TS_PYTHON_CONSTANTS_PATH,
         "// START PYTHON RELATED SETTINGS",
