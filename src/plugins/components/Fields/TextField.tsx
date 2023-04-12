d9d624ca7fb2ca66a64e7051d98db8b74280b0d1
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2023-04-12 10:52:44 +0800

    💄Enhanced `TextField`

--- src/plugins/components/Fields/TextField.tsx
+++ src/plugins/components/Fields/TextField.tsx
@@ -4,11 +4,15 @@ import { langStore, translate } from "@noli/business";
 
 import type { IField } from "@/types/plugins";
 import type { ITextField } from "@/types/metaFields";
+import { UI_Words } from "@/lang/ui";
 import { getMetaField, setMetaField } from "@/stores/meta";
 import { CFInput } from "@/components/CFInput";
 
 export interface TextFieldProps extends IField<ITextField> {}
 function TextField({ field, definition }: TextFieldProps) {
+  const lang = langStore.tgt;
+  const placeholderWord = `${field}-field-placeholder`;
+
   return (
     <CFInput
       value={getMetaField(field) as string}
@@ -16,7 +20,11 @@ function TextField({ field, definition }: TextFieldProps) {
         setMetaField(field, event.target.value);
         definition.props?.onChange?.(event);
       }}
-      placeholder={translate(`${field}-field-placeholder`, langStore.tgt)}
+      placeholder={
+        placeholderWord in UI_Words
+          ? translate(placeholderWord, lang)
+          : field.charAt(0).toUpperCase() + field.slice(1)
+      }
       {...definition.props}
     />
   );
