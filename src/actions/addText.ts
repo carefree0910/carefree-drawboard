dcc2a115690fbacc14eeb47fc489fcb0154c5591
carefree0910 <syameimaru.saki@gmail.com>
2023-04-14 22:08:48 +0800

    ✨Implemented `addNewText`

---
+++ src/actions/addText.ts
@@ -0,0 +1,36 @@
+import { BBox, TextNode } from "@noli/core";
+import { BoardStore, BusinessOpCallbacks, useSafeExecute } from "@noli/business";
+
+import type { IMeta } from "@/schema/meta";
+import { themeStore } from "@/stores/theme";
+
+export function addNewText(
+  alias: string,
+  content: string,
+  fontSize: number,
+  opt: {
+    bbox: BBox;
+    meta: IMeta;
+    callbacks?: BusinessOpCallbacks;
+    noSelect?: boolean;
+  },
+): void {
+  const newText = new TextNode(
+    alias,
+    { content, fontSize },
+    opt.bbox.fields,
+    { z: BoardStore.graph.minZIndex - 1 },
+    undefined,
+    undefined,
+    { color: themeStore.styles.textColor },
+  );
+  newText.meta = opt.meta;
+  console.log("newText: ", newText.snapshot());
+  useSafeExecute("addJson", null, true, opt.callbacks, {
+    noSelect: opt.noSelect ?? true,
+    safeOpt: {
+      retry: 3,
+      retryInterval: 500,
+    },
+  })({ alias, json: newText.toJson() });
+}
