8d969b871bbeb481d4f690e5e1e276bf277df0de
carefree0910 <syameimaru.saki@gmail.com>
2023-04-14 12:58:22 +0800

    🐛Fixed `getNodeFilter`

--- src/plugins/utils/renderFilters.ts
+++ src/plugins/utils/renderFilters.ts
@@ -6,7 +6,7 @@ export function getNodeFilter(constraint: NodeConstraints): (info?: IResponse) =
   return (info) => {
     if (constraint === "none") return true;
     if (!info) return false;
-    if (constraint === "anyNode") return info.type !== "multiple";
+    if (constraint === "anyNode") return info.type !== "none";
     if (constraint === "multiNode") return info.type === "multiple";
     if (constraint === "singleNode") return !["group", "multiple", "none"].includes(info.type);
     return info.type === constraint;
