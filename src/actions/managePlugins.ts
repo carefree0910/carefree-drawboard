a35e3402ad4f76a17fa402b202bbfde300e1ac62
carefree0910 <syameimaru.saki@gmail.com>
2023-04-16 11:43:07 +0800

    ⚡️Utilized `runInAction`

--- src/actions/managePlugins.ts
+++ src/actions/managePlugins.ts
@@ -1,15 +1,19 @@
+import { runInAction } from "mobx";
+
 import { reactPluginSettings } from "@/_settings";
 import { usePythonPluginSettings } from "@/stores/_python";
 import { setPluginVisible, setPythonPluginVisible } from "@/stores/plugins";
 
 function setAllPlugins(visible: boolean) {
-  reactPluginSettings.forEach(({ type }) => {
-    if (!["settings", "undo", "redo"].includes(type)) {
-      setPluginVisible(type, visible);
-    }
-  });
-  usePythonPluginSettings().forEach(({ props }) => {
-    setPythonPluginVisible(props.pluginInfo.identifier, visible);
+  runInAction(() => {
+    reactPluginSettings.forEach(({ type }) => {
+      if (!["settings", "undo", "redo"].includes(type)) {
+        setPluginVisible(type, visible);
+      }
+    });
+    usePythonPluginSettings().forEach(({ props }) => {
+      setPythonPluginVisible(props.pluginInfo.identifier, visible);
+    });
   });
 }
 
