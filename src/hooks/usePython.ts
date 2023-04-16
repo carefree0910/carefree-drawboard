d23adae8c99d022c9308e5881036dc5aaefa2099
carefree0910 <syameimaru.saki@gmail.com>
2023-04-16 10:37:03 +0800

    ✨Made `getNodeData` async

--- src/hooks/usePython.ts
+++ src/hooks/usePython.ts
@@ -3,12 +3,18 @@ import { useCallback, useEffect } from "react";
 import { INode, Logger } from "@noli/core";
 
 import type { IMeta } from "@/schema/meta";
-import type { INodeData, IPythonResponse, IPythonRequest, IUseHttpPython } from "@/schema/_python";
+import type {
+  INodeData,
+  IPythonResponse,
+  IPythonRequest,
+  IUseHttpPython,
+  IUsePythonInfo,
+} from "@/schema/_python";
 import { IPythonStore, updatePythonStore } from "@/stores/_python";
 import { Requests } from "@/requests/actions";
 import { useWebSocket } from "@/requests/hooks";
 
-function getNodeData(node: INode | null): INodeData {
+async function getNodeData(node: INode | null): Promise<INodeData> {
   if (!node) return {};
   const { x, y } = node.position;
   const { w, h } = node.wh;
@@ -16,8 +22,26 @@ function getNodeData(node: INode | null): INodeData {
   const text = node.type === "text" ? node.params.content : undefined;
   const src = node.type === "image" ? node.renderParams.src : undefined;
   const meta = (node.type === "group" ? undefined : node.params.meta) as IMeta | undefined;
-  const children = node.type === "group" ? node.nodes.map(getNodeData) : undefined;
+  const children = node.type === "group" ? await getNodeDataList(node.nodes) : undefined;
   return { type: node.type, x, y, w, h, transform, text, src, meta, children };
+}
+async function getNodeDataList(nodes: INode[]): Promise<INodeData[]> {
+  return Promise.all(nodes.map(getNodeData));
+}
+async function getPythonRequest({
+  node,
+  nodes,
+  identifier,
+  getExtraRequestData,
+}: Omit<IUsePythonInfo, "endpoint" | "isInvisible">): Promise<IPythonRequest> {
+  const nodeData = await getNodeData(node);
+  const nodeDataList = nodes.length <= 1 ? [] : await getNodeDataList(nodes);
+  return {
+    identifier,
+    nodeData,
+    nodeDataList,
+    extraData: getExtraRequestData ? getExtraRequestData() : {},
+  };
 }
 
 export function useHttpPython<R>({
@@ -38,14 +62,8 @@ export function useHttpPython<R>({
     if (isInvisible || forceNotSend) return Promise.resolve();
     const preprocess = beforeRequest ? beforeRequest() : Promise.resolve();
     return preprocess
-      .then(() =>
-        Requests.postJson<IPythonResponse<R>, IPythonRequest>("_python", endpoint, {
-          identifier,
-          nodeData: getNodeData(node),
-          nodeDataList: nodes.length <= 1 ? [] : nodes.map(getNodeData),
-          extraData: getExtraRequestData ? getExtraRequestData() : {},
-        }),
-      )
+      .then(() => getPythonRequest({ node, nodes, identifier, getExtraRequestData }))
+      .then((req) => Requests.postJson<IPythonResponse<R>>("_python", endpoint, req))
       .then((res) => {
         if (res.success) return onUseHttpPythonSuccess(res);
         throw Error(res.message);
