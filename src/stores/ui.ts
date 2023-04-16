d0f45a9f2f86eaba6a6834d292865b11f5bb6e5b
carefree0910 <syameimaru.saki@gmail.com>
2023-04-16 17:59:18 +0800

    ✨Implemented `VisibleManager`

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -1,6 +1,17 @@
-import { makeObservable, observable } from "mobx";
+import { makeObservable, observable, runInAction } from "mobx";
 
+import type { Dictionary } from "@noli/core";
 import { ABCStore } from "@noli/business";
+
+import type { AvailablePlugins } from "@/schema/plugins";
+import { reactPluginSettings } from "@/_settings";
+import {
+  pluginIsVisible,
+  pythonPluginIsVisible,
+  setPluginVisible,
+  setPythonPluginVisible,
+} from "./plugins";
+import { usePythonPluginSettings } from "./_python";
 
 export interface IUIStore {
   disablePluginSettings: boolean;
@@ -21,3 +32,42 @@ class UIStore extends ABCStore<IUIStore> implements IUIStore {
 }
 
 export const uiStore = new UIStore();
+
+export class VisibleManager {
+  static visibleBackup?: Dictionary<boolean>;
+  static pythonVisibleBackup?: Dictionary<boolean>;
+
+  static updateVisibleBackup() {
+    this.visibleBackup = {};
+    this.pythonVisibleBackup = {};
+    for (const { type } of reactPluginSettings) {
+      if (type !== "settings") {
+        this.visibleBackup[type] = pluginIsVisible(type);
+      }
+    }
+    for (const {
+      props: {
+        pluginInfo: { identifier },
+      },
+    } of usePythonPluginSettings()) {
+      this.pythonVisibleBackup[identifier] = pythonPluginIsVisible(identifier);
+    }
+  }
+
+  static restoreVisibleBackup() {
+    runInAction(() => {
+      if (this.visibleBackup) {
+        Object.entries(this.visibleBackup).forEach(([plugin, visible]) => {
+          setPluginVisible(plugin as AvailablePlugins, visible);
+        });
+      }
+      if (this.pythonVisibleBackup) {
+        Object.entries(this.pythonVisibleBackup).forEach(([identifier, visible]) => {
+          setPythonPluginVisible(identifier, visible);
+        });
+      }
+    });
+    this.visibleBackup = undefined;
+    this.pythonVisibleBackup = undefined;
+  }
+}
