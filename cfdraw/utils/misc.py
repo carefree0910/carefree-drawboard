33af0d01573c3345bdfdf45a2b559dd1c701e4b9
carefree0910 <syameimaru.saki@gmail.com>
2023-04-23 15:37:08 +0800

    🐛Followed `mypy`

--- cfdraw/utils/misc.py
+++ cfdraw/utils/misc.py
@@ -5,15 +5,15 @@ from cftool.misc import print_warning
 
 def deprecated(message: str) -> Callable[[type], type]:
     def _deprecated(cls: type) -> type:
-        def init(self, *args: Any, **kwargs: Any) -> None:
-            if not cls._warned_deprecation:
+        def init(self: Any, *args: Any, **kwargs: Any) -> None:
+            if not cls._warned_deprecation:  # type: ignore
                 print_warning(f"{cls.__name__} is deprecated, {message}")
-                cls._warned_deprecation = True
+                cls._warned_deprecation = True  # type: ignore
             original_init(self, *args, **kwargs)
 
-        cls._warned_deprecation = False
-        original_init = cls.__init__
-        cls.__init__ = init
+        cls._warned_deprecation = False  # type: ignore
+        original_init = cls.__init__  # type: ignore
+        cls.__init__ = init  # type: ignore
         return cls
 
     return _deprecated
