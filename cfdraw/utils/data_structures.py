12bb985e2ba450f410699e91e871123b0aa6c993
carefree0910 <syameimaru.saki@gmail.com>
2023-04-23 11:07:04 +0800

    ✨🚧Implemented `QueuesInQueue`

--- cfdraw/utils/data_structures.py
+++ cfdraw/utils/data_structures.py
@@ -75,6 +75,9 @@ class Bundle(Generic[TItemData]):
                 return item
         return None
 
+    def get_index(self, index: int) -> Item[TItemData]:
+        return self._items[index]
+
     def push(self, item: Item[TItemData]) -> None:
         if self.get(item.key) is not None:
             raise ValueError(f"item '{item.key}' already exists")
@@ -96,6 +99,83 @@ class Bundle(Generic[TItemData]):
                     self._items.pop(i)
                     break
         return item
+
+
+class QueuesInQueue(Generic[TItemData]):
+    def __init__(self, *, no_mapping: bool = True) -> None:
+        self._cursor = 0
+        self._queues: Bundle[Bundle[TItemData]] = Bundle(no_mapping=no_mapping)
+
+    def __iter__(self) -> Iterator[Item[Bundle[TItemData]]]:
+        return iter(self._queues)
+
+    @property
+    def is_empty(self) -> bool:
+        return self._queues.is_empty
+
+    @property
+    def num_queues(self) -> int:
+        return len(self._queues)
+
+    @property
+    def num_items(self) -> int:
+        return sum(len(q.data) for q in self._queues)
+
+    def get(self, queue_id: str) -> Optional[Item[Bundle[TItemData]]]:
+        return self._queues.get(queue_id)
+
+    def push(self, queue_id: str, item: Item[TItemData]) -> None:
+        queue_item = self._queues.get(queue_id)
+        if queue_item is not None:
+            queue = queue_item.data
+        else:
+            queue = Bundle()
+            self._queues.push(Item(queue_id, queue))
+        queue.push(item)
+
+    def next(self) -> Tuple[Optional[str], Optional[Item[TItemData]]]:
+        if self._queues.is_empty:
+            return None, None
+        self._cursor %= len(self._queues)
+        queue = self._queues.get_index(self._cursor)
+        item = queue.data.first
+        if item is None:
+            self._queues.remove(queue.key)
+            return self.next()
+        self._cursor += 1
+        return queue.key, item
+
+    def remove(self, queue_id: str, item_key: str) -> None:
+        queue_item = self._queues.get(queue_id)
+        if queue_item is None:
+            return
+        queue_item.data.remove(item_key)
+        if queue_item.data.is_empty:
+            self._queues.remove(queue_id)
+
+    def get_pending(self, item_key: str) -> Optional[List[Item[TItemData]]]:
+        if self._queues.is_empty:
+            return None
+        layer = 0
+        searched = False
+        pending: List[Item[TItemData]] = []
+        finished_searching = [False] * len(self._queues)
+        while not all(finished_searching):
+            for i, queue in enumerate(self._queues):
+                if finished_searching[i]:
+                    continue
+                if layer >= len(queue.data):
+                    finished_searching[i] = True
+                    continue
+                item = queue.data.get_index(layer)
+                if item.key == item_key:
+                    searched = True
+                    break
+                pending.append(item)
+            if searched:
+                break
+            layer += 1
+        return pending if searched else None
 
 
 class Types(Generic[TTypes]):
