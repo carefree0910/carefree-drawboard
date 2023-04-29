a3b60791990259aafa5770277505322dbd4b3dca
carefree0910 <syameimaru.saki@gmail.com>
2023-04-29 10:03:14 +0800

    🗑️Cleaned up redundant import

--- cfdraw/.web/src/stores/projects.ts
+++ cfdraw/.web/src/stores/projects.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import { makeObservable, observable, runInAction } from "mobx";
+import { makeObservable, observable } from "mobx";
 
 import { ABCStore } from "@carefree0910/business";
 
