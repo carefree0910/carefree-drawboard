38f973ae257d0c71ef09fdccd4baa28ea2050588
carefree0910 <syameimaru.saki@gmail.com>
2023-04-30 14:05:36 +0800

    ✨Introduced `CFTextarea`

---
+++ cfdraw/.web/src/components/CFTextarea.tsx
@@ -0,0 +1,12 @@
+import { observer } from "mobx-react-lite";
+import { Textarea, TextareaProps } from "@chakra-ui/react";
+
+import { themeStore } from "@/stores/theme";
+
+function CFTextarea(props: TextareaProps) {
+  const { textColor } = themeStore.styles;
+
+  return <Textarea color={textColor} {...props}></Textarea>;
+}
+
+export default observer(CFTextarea);
