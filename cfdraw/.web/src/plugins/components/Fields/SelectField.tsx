0c28ff34ec5235576e40056d16a0c91637132b53
carefree0910 <syameimaru.saki@gmail.com>
2023-04-30 17:02:15 +0800

    ✨Enhanced `SelectField`
    Supported `label`

--- cfdraw/.web/src/plugins/components/Fields/SelectField.tsx
+++ cfdraw/.web/src/plugins/components/Fields/SelectField.tsx
@@ -1,8 +1,10 @@
 import { observer } from "mobx-react-lite";
+import { Box, Flex } from "@chakra-ui/react";
 
 import type { IField } from "@/schema/plugins";
 import type { ISelectField } from "@/schema/fields";
 import { getMetaField, setMetaField } from "@/stores/meta";
+import { CFCaption } from "@/components/CFText";
 import { CFSrollableSelect } from "@/components/CFSelect";
 import { useDefaultFieldValue } from "./utils";
 
@@ -11,12 +13,17 @@ function SelectField({ field, definition }: SelectFieldProps) {
   useDefaultFieldValue({ field, definition });
 
   return (
-    <CFSrollableSelect
-      value={getMetaField(field) as string}
-      options={definition.values as string[]}
-      onOptionClick={(value) => setMetaField(field, value)}
-      {...definition.props}
-    />
+    <Flex w="100%" h="100%" align="center" {...definition.props}>
+      <CFCaption label={definition.label} />
+      <Box w="8px" />
+      <CFSrollableSelect
+        flex={1}
+        h="100%"
+        value={getMetaField(field) as string}
+        options={definition.values as string[]}
+        onOptionClick={(value) => setMetaField(field, value)}
+      />
+    </Flex>
   );
 }
 
