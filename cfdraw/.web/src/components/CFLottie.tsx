5f30c59ff248dc447fa1ac24d268ee4dedf0cd29
carefree0910 <syameimaru.saki@gmail.com>
2023-04-30 13:05:57 +0800

    ✨Enhanced `CFLottie`
    Supported `delay`, and now it will destroy itself when `hide`.

--- cfdraw/.web/src/components/CFLottie.tsx
+++ cfdraw/.web/src/components/CFLottie.tsx
@@ -1,24 +1,34 @@
 import lottie from "lottie-web";
-import React, { useEffect, useId } from "react";
+import React, { useCallback, useEffect, useId } from "react";
 import { Box, BoxProps } from "@chakra-ui/react";
 
 interface ICFLottie extends BoxProps {
   hide?: boolean;
+  delay?: number;
   animationData?: any;
 }
-const CFLottie: React.FC<ICFLottie> = ({ hide, animationData, id, ...others }) => {
+const CFLottie: React.FC<ICFLottie> = ({ hide, delay, animationData, id, ...others }) => {
   const generatedId = useId();
   const finalId = id ?? generatedId;
+  const loadLottie = useCallback(
+    () =>
+      lottie.loadAnimation({
+        container: document.getElementById(finalId)!,
+        animationData,
+      }),
+    [finalId, animationData],
+  );
 
   useEffect(() => {
-    if (hide) return;
-    lottie.loadAnimation({
-      container: document.getElementById(finalId)!,
-      animationData,
-    });
-  }, [finalId, hide, animationData]);
+    let timer: any;
+    const cleanup = () => clearTimeout(timer);
+    if (hide) return cleanup;
+    if (!delay) loadLottie();
+    else timer = setTimeout(loadLottie, delay);
+    return cleanup;
+  }, [finalId, hide, delay, animationData]);
 
-  return <Box id={finalId} {...others} />;
+  return <>{!hide && <Box id={finalId} {...others} />}</>;
 };
 
 export default CFLottie;
