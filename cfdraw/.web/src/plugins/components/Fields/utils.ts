a6f2b1861e9cb1d5f0f71f2f3fc961622309c96d
carefree0910 <syameimaru.saki@gmail.com>
2023-05-01 20:35:59 +0800

    🐛Fixed `useDefaultFieldValue`

--- cfdraw/.web/src/plugins/components/Fields/utils.ts
+++ cfdraw/.web/src/plugins/components/Fields/utils.ts
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 
+import { isUndefined } from "@carefree0910/core";
+
 import type { IField } from "@/schema/plugins";
 import type { IFieldDefinition } from "@/schema/fields";
 import { getMetaField, setMetaField } from "@/stores/meta";
 
 export function useDefaultFieldValue({ field, definition }: IField<IFieldDefinition>) {
   useEffect(() => {
-    if (!getMetaField(field)) {
+    if (isUndefined(getMetaField(field))) {
       setMetaField(field, definition.default);
     }
   }, [field, definition]);
