ee5cdc4c05469c6747a1d39baaf4d221b98400f9
carefree0910 <syameimaru.saki@gmail.com>
2023-05-02 22:45:56 +0800

    ✨Enhanced `setPluginNeedRender`
    Should collapse plugin when not `needRender`

--- cfdraw/.web/src/stores/pluginNeedRender.ts
+++ cfdraw/.web/src/stores/pluginNeedRender.ts
@@ -1,7 +1,9 @@
-import { makeObservable, observable } from "mobx";
+import { makeObservable, observable, runInAction } from "mobx";
 
 import type { Dictionary } from "@carefree0910/core";
 import { ABCStore, useIsReady } from "@carefree0910/business";
+
+import { setPluginExpanded } from "./pluginExpanded";
 
 export type IPluginsNeedRender = Dictionary<boolean>;
 class PluginsNeedRenderStore extends ABCStore<IPluginsNeedRender> {
@@ -22,5 +24,11 @@ class PluginsNeedRenderStore extends ABCStore<IPluginsNeedRender> {
 const pluginsNeedRenderStore = new PluginsNeedRenderStore();
 export const usePluginNeedRender = (id: string) =>
   useIsReady() && (pluginsNeedRenderStore.needRender[id] ?? false);
-export const setPluginNeedRender = (id: string, needRender: boolean) =>
-  pluginsNeedRenderStore.updateProperty(id, needRender);
+export const setPluginNeedRender = (id: string, needRender: boolean) => {
+  runInAction(() => {
+    pluginsNeedRenderStore.updateProperty(id, needRender);
+    if (!needRender) {
+      setPluginExpanded(id, false);
+    }
+  });
+};
