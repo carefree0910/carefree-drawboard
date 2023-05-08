39067295a39bcb9d8f5b794a688f49a8a56ee612
carefree0910 <syameimaru.saki@gmail.com>
2023-05-08 11:52:33 +0800

    ✅Introduced `test_chat`

---
+++ tests/test_chat.py
@@ -0,0 +1,41 @@
+from cfdraw import *
+
+
+class ChatPlugin(IChatPlugin):
+    requirements = ["lorem-text"]
+    notification = "A plugin that generates lorem ipsum chat."
+
+    @property
+    def settings(self) -> IPluginSettings:
+        return IPluginSettings(
+            w=600,
+            h=400,
+            useModal=True,
+            tooltip="Pseudo Chat",
+            pivot=PivotType.RIGHT,
+            pluginInfo=IChatPluginInfo(
+                closeOnSubmit=False,
+                toastOnSubmit=False,
+            ),
+        )
+
+    async def process(self, data: ISocketRequest) -> str:
+        import time
+        from lorem_text import lorem
+
+        request = data.extraData
+        context = request["context"]
+        userInput = request["userInput"]
+        if context:
+            context += "\n"
+        new_context = f"{context}\nYou: {userInput}\n\nBot: "
+        text = lorem.words(20)
+        for char in text:
+            new_context += char
+            self.send_progress(textList=[new_context])
+            time.sleep(0.025)
+        return new_context
+
+
+register_plugin("chat")(ChatPlugin)
+app = App()
