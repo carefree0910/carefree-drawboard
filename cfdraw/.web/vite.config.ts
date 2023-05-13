cd25e15eb67b2c83e480ec999aec0544506f88d0
carefree0910 <syameimaru.saki@gmail.com>
2023-05-13 12:27:57 +0800

    🚑️⚡️Prevented adding hash during build
    To make it load-balance friendly.

--- cfdraw/.web/vite.config.ts
+++ cfdraw/.web/vite.config.ts
@@ -46,6 +46,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
         manualChunks: {
           axios: ["axios"],
           jszip: ["jszip"],
