8201dd0693284f279fe1898828c1a85c80300029
carefree0910 <syameimaru.saki@gmail.com>
2023-05-17 22:21:35 +0800

    🔧hash can now be added because we can pre-build assets
    Thanks to the support of runtime envs.

--- cfdraw/.web/vite.config.ts
+++ cfdraw/.web/vite.config.ts
@@ -50,9 +50,6 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name].[ext]",
         manualChunks: {
           axios: ["axios"],
           jszip: ["jszip"],
