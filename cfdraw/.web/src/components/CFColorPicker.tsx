89e695e6a143bdd20b31519d9c45a03b10936f6e
carefree0910 <syameimaru.saki@gmail.com>
2023-05-18 18:25:37 +0800

    ✨🚧Implemented `CFColorPicker`

---
+++ cfdraw/.web/src/components/CFColorPicker.tsx
@@ -0,0 +1,96 @@
+import React, { useMemo } from "react";
+import { observer } from "mobx-react-lite";
+import {
+  Box,
+  ButtonProps,
+  FormControl,
+  FormControlProps,
+  FormLabelProps,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Portal,
+  Spacer,
+} from "@chakra-ui/react";
+import { ChromePicker, ChromePickerProps } from "react-color";
+
+import { getRandomHash } from "@carefree0910/core";
+
+import { themeStore } from "@/stores/theme";
+import CFTooltip, { CFFormLabel } from "./CFTooltip";
+
+interface IColorPicker {
+  pickerProps?: ChromePickerProps;
+  thumbnailProps?: ButtonProps;
+}
+const ColorPicker: React.FC<IColorPicker> = ({ pickerProps, thumbnailProps }) => {
+  const color = pickerProps?.color;
+
+  if (!color) return null;
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Box
+          as="button"
+          w="32px"
+          h="32px"
+          bg="white"
+          position="relative"
+          borderWidth="4px"
+          borderColor="#ddd"
+          borderRadius="6px"
+          {...thumbnailProps}>
+          <Box
+            w="100%"
+            h="100%"
+            borderWidth="2px"
+            borderRadius="2px"
+            borderColor="transparent"
+            bg={color.toString()}
+          />
+        </Box>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent w="100%" h="100%">
+          <ChromePicker {...pickerProps} />
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  );
+};
+
+interface ICFColorPicker extends ChromePickerProps {
+  label?: string;
+  tooltip?: string;
+  formProps?: {
+    label?: FormLabelProps;
+    control?: FormControlProps;
+  };
+  thumbnailProps?: ButtonProps;
+}
+function CFColorPicker({ label, tooltip, formProps, thumbnailProps, ...props }: ICFColorPicker) {
+  const hashId = useMemo(() => getRandomHash().toString(), []);
+
+  if (!label) {
+    return (
+      <CFTooltip label={tooltip}>
+        <ColorPicker pickerProps={props} thumbnailProps={thumbnailProps} />
+      </CFTooltip>
+    );
+  }
+  return (
+    <FormControl display="flex" alignItems="center" {...formProps?.control}>
+      <CFFormLabel
+        label={label}
+        tooltip={{ label: tooltip }}
+        {...formProps?.label}
+        htmlFor={hashId}
+      />
+      <Spacer />
+      <ColorPicker pickerProps={props} thumbnailProps={thumbnailProps} />
+    </FormControl>
+  );
+}
+
+export default observer(CFColorPicker);
