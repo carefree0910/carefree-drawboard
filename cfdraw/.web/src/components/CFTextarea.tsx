12d0e886856b40f3f8be96205c6bc354dc1fe937
carefree0910 <syameimaru.saki@gmail.com>
2023-05-22 22:27:08 +0800

    💄Enhanced `CFTextarea` with `useInputProps`

--- cfdraw/.web/src/components/CFTextarea.tsx
+++ cfdraw/.web/src/components/CFTextarea.tsx
@@ -1,18 +1,16 @@
 import { observer } from "mobx-react-lite";
 import { Textarea, TextareaProps } from "@chakra-ui/react";
 
-import { themeStore } from "@/stores/theme";
+import { useInputProps } from "@/stores/theme";
 import CFTooltip from "./CFTooltip";
 
 interface ICFTextarea extends TextareaProps {
   tooltip?: string;
 }
 function CFTextarea({ tooltip, ...props }: ICFTextarea) {
-  const { textColor, captionColor } = themeStore.styles;
-
   return (
     <CFTooltip label={tooltip}>
-      <Textarea color={textColor} _placeholder={{ color: captionColor }} {...props} />
+      <Textarea {...useInputProps()} {...props} />
     </CFTooltip>
   );
 }
