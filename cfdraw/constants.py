9c2c621bff3639f10364671aefbddbb5d17b2cfd
carefree0910 <syameimaru.saki@gmail.com>
2023-05-29 23:12:12 +0800

    🍱Added some icons

--- cfdraw/constants.py
+++ cfdraw/constants.py
@@ -96,5 +96,7 @@ SOD_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboar
 INPAINTING_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboard/icons/inpainting.svg"
 VARIATION_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboard/icons/variation.svg"
 HARMONIZATION_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboard/icons/sparkle.svg"
+WORKFLOW_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboard/icons/workflow.svg"
+EXECUTE_WORKFLOW_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboard/icons/execute-workflow.svg"
 DEFAULT_PLUGIN_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboard/icons/default-plugin.svg"
 DEFAULT_PLUGIN_GROUP_ICON = "https://ailab-huawei-cdn.nolibox.com/upload/static/carefree-drawboard/icons/default-plugin-group.svg"
