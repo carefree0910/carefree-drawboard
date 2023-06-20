f99b124f5d0d4ea60c78f721c4fb2f23123e909f
carefree0910 <syameimaru.saki@gmail.com>
2023-06-20 11:01:13 +0800

    ✨Supported `CUSTOM_INTERCEPTORS`

--- cfdraw/.web/src/requests/interceptors/index.ts
+++ cfdraw/.web/src/requests/interceptors/index.ts
@@ -3,13 +3,17 @@ import type { AxiosInstance } from "axios";
 import type { APISources, Interceptors } from "@/schema/requests";
 import { useDefaultInceptors } from "./_default";
 
+const CUSTOM_INTERCEPTORS: Partial<Record<APISources, Interceptors>> = {
+  _python: useDefaultInceptors("Python"),
+};
+
 export function useInceptors(
   api: AxiosInstance,
   source: APISources,
   interceptors?: Interceptors,
 ): void {
   const { beforeRequest, requestError, beforeResponse, responseError } =
-    interceptors ?? useDefaultInceptors(source);
+    interceptors ?? CUSTOM_INTERCEPTORS[source] ?? useDefaultInceptors(source);
   api.interceptors.request.use(
     beforeRequest ?? ((config) => config),
     requestError ?? ((error) => Promise.reject(error)),
