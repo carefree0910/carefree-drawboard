22c02e9442b93650790f9a692fb3b0c5070ce914
carefree0910 <syameimaru.saki@gmail.com>
2023-07-12 09:01:20 +0800

    🐛Followed latest API

--- cfdraw/.web/src/actions/update.ts
+++ cfdraw/.web/src/actions/update.ts
@@ -1,6 +1,6 @@
 import type { Dictionary } from "@carefree0910/core";
-import { useSafeExecute } from "@carefree0910/business";
+import { BoardStore, useSafeExecute } from "@carefree0910/business";
 
 export function updateMeta(alias: string, meta: Dictionary<any>, trace: boolean = true) {
-  useSafeExecute("updateMeta", null, trace)({ alias, meta });
+  useSafeExecute("updateMeta", BoardStore.graph.getExistingNode(alias), trace)(meta);
 }
