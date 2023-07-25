ecc99ba5aac663d4d9a8db97c372dd5aacd27542
carefree0910 <syameimaru.saki@gmail.com>
2023-07-25 14:29:17 +0800

    🗑️✨Utilized `NoliFrameContentEditor` from `components`

--- cfdraw/.web/src/plugins/_react/NoliFrameEditorPlugin.tsx
+++ cfdraw/.web/src/plugins/_react/NoliFrameEditorPlugin.tsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { Flex } from "@chakra-ui/react";
 
-import { langStore, selectingNodesStore, translate, useEditNode } from "@carefree0910/business";
-import { BasicEditor } from "@carefree0910/components";
+import { langStore, selectingNodesStore, translate } from "@carefree0910/business";
+import { BasicEditor, NoliFrameContentEditor } from "@carefree0910/components";
 
 import type { IPlugin } from "@/schema/plugins";
 import { NodeEditor_Words } from "@/lang/nodeEditor";
 import { usePluginIds } from "@/stores/pluginsInfo";
 import CFDivider from "@/components/CFDivider";
 import CFHeading from "@/components/CFHeading";
-import CFTextarea from "@/components/CFTextarea";
 import { drawboardPluginFactory } from "../utils/factory";
 import Render from "../components/Render";
 
@@ -23,13 +21,6 @@ const NoliFrameEditorPlugin = ({ pluginInfo: { node }, ...props }: IPlugin) => {
     props.renderInfo.isInvisible = true;
   }
 
-  const [content, setContent] = useState("");
-  useEffect(() => {
-    if (content !== apiInfo?.content) setContent(apiInfo?.content ?? "");
-  }, [apiInfo]);
-
-  const { editNoliFrame } = useEditNode({ allowUsePreviousNode: true });
-
   return (
     <Render id={id} {...props}>
       <CFHeading>{translate(NodeEditor_Words["noliFrame-editor-plugin-header"], lang)}</CFHeading>
@@ -37,12 +28,7 @@ const NoliFrameEditorPlugin = ({ pluginInfo: { node }, ...props }: IPlugin) => {
       <BasicEditor />
       <CFDivider />
       <Flex direction="column" w="100%" h="100%">
-        <CFTextarea
-          flex={1}
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          onBlur={() => editNoliFrame({ trace: true })({ content })}
-        />
+        <NoliFrameContentEditor flex={1} />
       </Flex>
     </Render>
   );
