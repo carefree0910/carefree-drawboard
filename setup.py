c058e0c2ab03b72bacab15e067da4c9009adfc7e
carefree0910 <syameimaru.saki@gmail.com>
2023-10-16 09:51:05 +0800

    📦️Bumped version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.2-alpha.0"
+VERSION = "0.0.3-alpha.0"
 PACKAGE_NAME = "carefree-drawboard"
 
 DESCRIPTION = "🎨 Infinite Drawboard in Python"
